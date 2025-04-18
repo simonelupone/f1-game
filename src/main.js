1ce595e3c53756f37110b182de2de21cb0e3100c
simonelupone <simone.lupone@gmail.com>
2025-04-18 15:47:26 +0200

    feat: add list items generation in getDrivers function

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { getDrivers } from './components/drivers.js'
+import { getDrivers } from './modules/drivers.js';
+import { createDriversSelect } from './components/driversSelect.js';
+import { createDriversList } from './components/driversList.js';
+
+const driversSelect = document.getElementById('drivers-select');
+const driversList = document.getElementById('drivers-list');
 
 /**
  * @typedef {Object} Driver
@@ -6,12 +11,14 @@ import { getDrivers } from './components/drivers.js'
  * @property {number} driver_number - driver's car number
  */
 getDrivers().then(drivers => {
-    const driverSelect = document.getElementById('driver-select');
+    if (driversSelect && driversList && drivers.length > 0) {
 
-    if (driverSelect && drivers.length > 0) {
-        const driverOptions = drivers.map(driver => `<option class="dark:bg-red-700" value="${driver.driver_number}">${driver.broadcast_name}</option>`).join('');
-        driverSelect.innerHTML = driverOptions;
+        const driversOptions = drivers.map(createDriversSelect).join('');
+        const driversListItems = drivers.map(createDriversList).join('');
+
+        driversSelect.innerHTML = driversOptions;
+        driversList.innerHTML = driversListItems;
     } else {
-        driverSelect.innerHTML = '<p>Nessun pilota trovato.</p>';
+        driversSelect.innerHTML = '<p>Nessun pilota trovato.</p>';
     }
 });
