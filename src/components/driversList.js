e5c90b6f94506023885e4bc37af7876a46d276e9
simonelupone <simone.lupone@gmail.com>
2025-04-18 19:47:50 +0200

    feat: add selected list item style condition and jsdoc

--- src/components/driversList.js
+++ src/components/driversList.js
@@ -1,5 +1,13 @@
-export const createDriversList = ({ broadcast_name }) => {
-    return `
-    <li class="text-xl">${broadcast_name}</li>
-    `;
-}
+/**
+ * Creates a list item for a driver
+ * Applies bold styling if the driver is selected
+ * @param {Object} driver - The driver object
+ * @param {string} driver.broadcast_name - The driver's display name
+ * @param {boolean} driver.selected - Whether the driver is selected
+ * @returns {string} HTML string for the list item
+ */
+export const createDriversList = ({ broadcast_name, selected }) => {
+    return selected
+        ? `<li class="text-xl text-red-500 font-f1-normal-700">${broadcast_name}</li>`
+        : `<li class="text-xl font-f1-normal-400">${broadcast_name}</li>`;
+};
