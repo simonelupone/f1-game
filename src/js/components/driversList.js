e9b9f7e5015d1613e5e56eb3cfcbf96088c544c9
simonelupone <simone.lupone@gmail.com>
2025-04-26 16:07:27 +0200

    fix: delete table td and maintain only driver data

--- src/js/components/driversList.js
+++ src/js/components/driversList.js
@@ -1,27 +1,13 @@
-export const createDriverRow = ({ last_name, team_colour, selected }, index) => {
+export const createDrivers = ({ last_name, team_colour, selected }, index) => {
     const position = index + 1;
     return `
-        <tr class="${selected
-            ? 'bg-red-200 dark:bg-red-800/40'
-            : 'odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800'} 
-            border-b dark:border-gray-700 border-gray-200">
-            
             <td
-                class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap text-center dark:text-white">
+                class="w-32 px-6 py-2 font-medium text-gray-900 whitespace-nowrap text-center dark:text-white">
                 <div class="flex items-center gap-2">
                     <span class="w-6 font-f1-normal-700">${position}</span>
                     <div class="w-1.5 h-4.5 block" style="background: #${team_colour}"></div>
                     <span class="font-f1-normal-400 uppercase">${last_name}</span>
                 </div>
             </td>
-            <td class="text-lg font-bold px-6 py-2">
-                
-            </td>
-            <td class="px-6 py-2"></td>
-            <td class="px-6 py-2"></td>
-            <td class="px-6 py-2 w-10">
-                <img src="" alt="">
-            </td>
-        </tr>
     `;
 };
