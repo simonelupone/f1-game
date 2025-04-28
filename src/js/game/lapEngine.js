51a7228545c6a3d45520ab52222e77876d2733a2
simonelupone <simone.lupone@gmail.com>
2025-04-28 17:55:35 +0200

    feat: add Math.abs to ensure input is positive

--- src/js/game/lapEngine.js
+++ src/js/game/lapEngine.js
@@ -4,11 +4,13 @@ export const lap = (baseTime = 90000, variance = 3000) => {
 }
 
 export const formatLapTime = (timeMs) => {
-    if(!timeMs) return '';
-    const minutes = Math.floor(timeMs / 60000);
-    const seconds = Math.floor((timeMs % 60000) / 1000);
-    const milliseconds = Math.floor(timeMs % 1000);
-    if(timeMs >= 60000) {
+    if(!timeMs && timeMs !== 0) return '';
+    // use Math.abs to ensure input time is positive (to avoid format like 0,-014)
+    const absTime = Math.abs(timeMs);
+    const minutes = Math.floor(absTime / 60000);
+    const seconds = Math.floor((absTime % 60000) / 1000);
+    const milliseconds = Math.floor(absTime % 1000);
+    if(absTime >= 60000) {
         return `${minutes}:${seconds.toString().padStart(2, '0')}.${milliseconds.toString().padStart(3, '0')}`;
     } else {
         return `${seconds.toString().padStart(1, '0')}.${milliseconds.toString().padStart(3, '0')}`;
